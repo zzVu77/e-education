77193b9a927cdb636fc1cce1b37085cbed9c5b3a
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-07-15 17:34:22 +0700

    feat: wrap SearchBar and Filter components in Suspense for improved loading handling

--- src/components/SearchAndFilterSection.tsx
+++ src/components/SearchAndFilterSection.tsx
@@ -2,14 +2,21 @@ import Filter from "@/components/Filter";
 
 import SearchBar from "@/components/SearchBar";
 import { dummyCategories, dummyPriceRanges } from "../../constants/data";
+import { Suspense } from "react";
 const SearchAndFilterSection = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row items-start md:items-center justify-start gap-2 w-full">
-        <SearchBar />
+        <Suspense fallback={null}>
+          <SearchBar />
+        </Suspense>
         <div className="flex flex-row items-center justify-center gap-2 w-full md:w-auto">
-          <Filter typeOfFilter="category" items={dummyCategories} />
-          <Filter typeOfFilter="price" items={dummyPriceRanges} />
+          <Suspense fallback={null}>
+            <Filter typeOfFilter="category" items={dummyCategories} />
+          </Suspense>
+          <Suspense fallback={null}>
+            <Filter typeOfFilter="price" items={dummyPriceRanges} />
+          </Suspense>
         </div>
       </div>
     </>
