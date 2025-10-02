79a37d4e894018e210d79e74aa483d2b6fbf7f8e
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-02 16:05:34 +0700

    refactor: remove unused imports and comment out components in Page

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,16 +1,12 @@
-import CustomPagination from "@/components/CustomPagination";
-import ProductSkeleton from "@/components/ProductSkeleton";
-import SearchAndFilterSection from "@/components/SearchAndFilterSection";
-
 const Page = () => {
   return (
     <div>
       {/* <Button onClick={handleClickLogin}>Login</Button>
       <Button onClick={handleClickLogout}>Logout</Button>
       <Button onClick={handleClickFetchCourses}>Fetch Courses</Button> */}
-      <ProductSkeleton />
+      {/* <ProductSkeleton />
       <SearchAndFilterSection />
-      <CustomPagination totalPages={20} />
+      <CustomPagination totalPages={20} /> */}
     </div>
   );
 };
