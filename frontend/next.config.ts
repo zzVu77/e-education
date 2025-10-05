616a1b0f792b459602f55415d2ea66950a5c4b73
giahuynguyen28 <giahuy902@gmail.com>
2025-10-05 18:23:46 +0700

    feat: add Cloudinary to image domains in Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["placehold.co", "images.unsplash.com"],
+    domains: ["placehold.co", "images.unsplash.com", "res.cloudinary.com"],
   },
 };
 
